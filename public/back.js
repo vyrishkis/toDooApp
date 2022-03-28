a93fff4af85b9aa1711223fb594bbcd48bdd3db8
vyrishkis <krasovskis@gmail.com>
2022-03-28 14:55:26 +0300

    Tailwind install

---
+++ public/back.js
@@ -0,0 +1,16 @@
+let ourForm = document.getElementById("ourForm")
+let ourField = document.getElementById("ourField")
+let ourList = document.getElementById("ourList")
+
+ourForm.addEventListener("submit",(e)=>{
+  e.preventDefault() // neleidzhia siusti duomenu i serveri
+  createItem(ourField.value)
+})
+
+function createItem(x){
+ 
+ let ourHTML = `<li> ${x} <button>Delete</button></li>`
+ 
+ ourList.insertAdjacentHTML("beforeend",ourHTML)
+ 
+}
